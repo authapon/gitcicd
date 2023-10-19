03d74a510c11f7b5407161b5e630fa3093447e42
Authapon Kongwan <authapon@gmail.com>
2023-10-19 15:11:21 +0700

    update

--- main.go
+++ main.go
@@ -44,21 +44,22 @@ func cicd(c *fiber.Ctx) error {
 	data := make(map[string]any)
 	err := json.Unmarshal(c.Body(), &data)
 	if err != nil {
-		log.Printf("%v\n", err)
+		log.Printf("cicd: error -> %v\n", err)
 		return err
 	}
 	message := data["head_commit"].(map[string]any)["message"].(string)
 	name := data["repository"].(map[string]any)["name"].(string)
 	owner := data["repository"].(map[string]any)["owner"].(map[string]any)["name"].(string)
-	log.Printf("process owner:%s - name:%s - message:%s\n", owner, name, message)
+	log.Printf("cicd: process owner:%s - name:%s - message:%s\n", owner, name, message)
 	for i := range conf.Condition {
 		if owner == conf.Condition[i].Owner && name == conf.Condition[i].Name && message == conf.Condition[i].Message {
 			for i2 := range conf.Condition[i].Script {
 				odata, err := runCom(conf.Condition[i].Script[i2])
 				if err != nil {
-					log.Printf("error: %v\n", err)
+					log.Printf("cicd: error -> %v\n", err)
 					return err
 				}
+				log.Printf("cicd: " + string(odata))
 				output += string(odata) + "\n"
 			}
 		}
@@ -70,7 +71,7 @@ func cicd(c *fiber.Ctx) error {
 func logcicd(c *fiber.Ctx) error {
 	output, err := runCom("sudo", "systemctl", "status", "gitcicd", "-n", "100")
 	if err != nil {
-		log.Printf("error: %v \n", err)
+		log.Printf("cicd: error -> %v \n", err)
 		return err
 	}
 	return c.SendString(string(output))

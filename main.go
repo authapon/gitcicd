3421ffa59730a2b260653da864e926a174fe956f
Authapon Kongwan <authapon@gmail.com>
2023-10-19 22:07:11 +0700

    add support gitlab

--- main.go
+++ main.go
@@ -12,15 +12,16 @@ import (
 
 type (
 	ConditionBuild struct {
-		Owner   string   `json:"owner"`
-		Name    string   `json:"name"`
-		Message string   `json:"message"`
-		Ref     string   `json:"ref"`
-		Script  []string `json:"script"`
+		Typex   string   `json:"type" yaml:"type"`
+		Owner   string   `json:"owner" yaml:"owner"`
+		Name    string   `json:"name" yaml:"name"`
+		Message string   `json:"message" yaml:"message"`
+		Ref     string   `json:"ref" yaml:"ref"`
+		Script  []string `json:"script" yaml:"script"`
 	}
 	Conf struct {
-		Listen    string           `json:"listen"`
-		Condition []ConditionBuild `json:"condition"`
+		Listen    string           `json:"listen" yaml:"listen"`
+		Condition []ConditionBuild `json:"condition" yaml:"condition"`
 	}
 )
 
@@ -28,6 +29,7 @@ var conf Conf
 
 func main() {
 	processConfig()
+	log.Printf("cicd: %v\n", conf)
 	app := fiber.New(fiber.Config{
 		ProxyHeader:    fiber.HeaderXForwardedFor,
 		ReadBufferSize: 20480,
@@ -41,29 +43,54 @@ func main() {
 }
 
 func cicd(c *fiber.Ctx) error {
+	name := ""
+	owner := ""
+	ref := ""
+	message := ""
+	typex := ""
+
+	log.Printf("cicd: start!!!\n")
+
 	data := make(map[string]any)
 	err := json.Unmarshal(c.Body(), &data)
 	if err != nil {
 		log.Printf("cicd: error -> %v\n", err)
 		return err
 	}
-	message := data["head_commit"].(map[string]any)["message"].(string)
-	name := data["repository"].(map[string]any)["name"].(string)
-	owner := data["repository"].(map[string]any)["owner"].(map[string]any)["name"].(string)
-	ref := data["ref"].(string)
-	log.Printf("cicd: process owner:%s - name:%s - ref:%s - message:%s\n", owner, name, ref, message)
+	owner, ok := data["user_username"].(string)
+	if !ok {
+		typex = "github"
+		ref = data["ref"].(string)
+		name = data["repository"].(map[string]any)["name"].(string)
+		owner = data["repository"].(map[string]any)["owner"].(map[string]any)["name"].(string)
+		message = data["head_commit"].(map[string]any)["message"].(string)
+
+	} else {
+		typex = "gitlab"
+		ref = data["ref"].(string)
+		name = data["repository"].(map[string]any)["name"].(string)
+		lx := len(data["commits"].([]any))
+		if lx > 0 {
+			message = data["commits"].([]any)[lx-1].(map[string]any)["title"].(string)
+		}
+	}
+
+	log.Printf("cicd: process type:%s - owner:%s - name:%s - ref:%s - message:%s\n", typex, owner, name, ref, message)
 	for i := range conf.Condition {
-		if owner == conf.Condition[i].Owner && name == conf.Condition[i].Name && message == conf.Condition[i].Message && ref == conf.Condition[i].Ref {
+		log.Printf("cicd: test type:%s - owner:%s - name:%s - ref:%s - message:%s\n", conf.Condition[i].Typex, conf.Condition[i].Owner, conf.Condition[i].Name, conf.Condition[i].Ref, conf.Condition[i].Message)
+		if owner == conf.Condition[i].Owner && name == conf.Condition[i].Name && message == conf.Condition[i].Message && ref == conf.Condition[i].Ref && typex == conf.Condition[i].Typex {
+			log.Printf("cicd: activated!\n")
 			for i2 := range conf.Condition[i].Script {
-				odata, err := runCom(conf.Condition[i].Script[i2])
+				log.Printf("cicd: exec %s\n", conf.Condition[i].Script[i2])
+				_, err := runCom(conf.Condition[i].Script[i2])
 				if err != nil {
 					log.Printf("cicd: error -> %v\n", err)
 					return err
 				}
-				log.Printf("cicd: " + string(odata) + "\n")
 			}
 		}
 	}
+	log.Printf("cicd: finished\n")
 
 	return c.SendString("ok")
 }

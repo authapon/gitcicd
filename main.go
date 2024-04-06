b9e70463bde026d0f8d868300ab3ec8491cd3907
Authapon Kongwan <authapon@gmail.com>
2024-04-06 11:57:09 +0700

    update logs message

--- main.go
+++ main.go
@@ -58,9 +58,11 @@ func readConfig() {
 	if utils.FileExist("/etc/cicd.hcl") {
 		utils.ProcessConfigHCL("/etc/cicd.hcl", &conf)
 		log.Printf("Load /etc/cicd.hcl sucessfully\n")
+		logs.AddLog("load hcl configuration successfull\n")
 	} else if utils.FileExist("/etc/cicd.yml") {
 		utils.ProcessConfig("/etc/cicd.yml", &conf)
 		log.Printf("Load /etc/cicd.yml sucessfully\n")
+		logs.AddLog("load yml configuration successfull\n")
 	} else {
 		log.Printf("Error: cannot load configurationfile\n")
 		return
